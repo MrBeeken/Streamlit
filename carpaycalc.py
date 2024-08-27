0d33a463a0616ee9aaee20ddeb77bb557a5a1418
MrBeeken <beekenrikus@gmail.com>
2024-08-27 23:38:30 +0200

    carpaycalc.py

--- carpaycalc.py
+++ carpaycalc.py
@@ -3,14 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-st.title("Mortgage Repayments Calculator")
+st.title("Car Payment Calculator")
 
 st.write("### Input Data")
 col1, col2 = st.columns(2)
-home_value = col1.number_input("Home Value", min_value=0, value=500000)
-deposit = col1.number_input("Deposit", min_value=0, value=100000)
-interest_rate = col2.number_input("Interest Rate (in %)", min_value=0.0, value=5.5)
-loan_term = col2.number_input("Loan Term (in years)", min_value=1, value=30)
+home_value = col1.number_input("Car Value", min_value=0, value=2000000)
+deposit = col1.number_input("Deposit", min_value=0, value=400000)
+interest_rate = col2.number_input("Interest Rate %", min_value=0.0, value=15.5)
+loan_term = col2.number_input("Loan Term in years", min_value=1, value=30)
 
 # Calculate the repayments.
 loan_amount = home_value - deposit
