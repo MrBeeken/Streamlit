cf7fb6dc5750f4d1c0076b826052ea1ed6ddb729
MrBeeken <beekenrikus@gmail.com>
2024-08-27 23:45:25 +0200

    carpaycalc.py

--- carpaycalc.py
+++ carpaycalc.py
@@ -17,9 +17,11 @@ term = col2.number_input("Term in years", min_value=1, max_value=15)
 loan_amount = car_value - deposit
 monthly_interest_rate = (interest_rate/100)/12
 number_of_payments = term*12
-monthly_payment_cost = (loan_amount * (monthly_interest_rate * (1 + monthly_interest_rate) ** number_of_payments)
-                        / ((1 + monthly_interest_rate) ** number_of_payments -1)
-                        )
+monthly_payment_cost = (
+    loan_amount
+    * (monthly_interest_rate * (1 + monthly_interest_rate) ** number_of_payments)
+    / ((1 + monthly_interest_rate) ** number_of_payments - 1)
+)
 
 #Section to Display Graph
 total_payments = monthly_payment_cost * number_of_payments
